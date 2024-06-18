c335aca55748fe3df50e31e1da7ead2109901f67
Tejashwini Rao <polepalle.07@gmail.com>
2024-06-18 21:48:37 +0530

    Pushing all components-shell

--- create.sh
+++ create.sh
@@ -26,7 +26,7 @@ create_ec2() {
 }
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-AMI_ID=ami-05e3db669169a67c8
+AMI_ID=ami-0679e2f4396cdb59e
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 
