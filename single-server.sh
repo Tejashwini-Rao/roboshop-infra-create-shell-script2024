1c4c62804d6079186d60b72a5d9536535e19db72
Tejashwini Rao <polepalle.07@gmail.com>
2024-06-18 22:12:46 +0530

    Pushing all components-shell

---
+++ single-server.sh
@@ -0,0 +1,29 @@
+##### Change these values ###
+ZONE_ID="Z00593593U4GYH63O0YNE"
+SG_NAME="Allow-All"
+ENV="dev"###################
+
+
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id ${AMI_ID} \
+      --instance-type t2.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --security-group-ids ${SGID} \
+      --iam-instance-profile Name=SecretManager_role_Roboshop \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+      echo Server IP Address = ${PRIVATE_IP}
+}
+
+#AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=ami-0679e2f4396cdb59e
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+if [ -z "$1" ]; then
+  echo "Input the node name"
+  exit 1
+else
+  COMPONENT=$1
+  create_ec2
+fi
